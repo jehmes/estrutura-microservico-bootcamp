9a3ef0ec5ad765035a0ad7684ae478a84f5a7b37
Jehmes Thales <jeihmesthales@hotmail.com>
2022-10-26 20:51:46 -0300

    Configuration CORS

--- ms-product/src/main/java/com/br/controllers/ProductController.java
+++ ms-product/src/main/java/com/br/controllers/ProductController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import com.br.services.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/product-service")
 @RestController
 @Tag(name = "Product Service API")
