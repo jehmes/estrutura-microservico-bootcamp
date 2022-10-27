53b2dc1df3c21c869adf53449136a07979d1f063
Jehmes Thales <jeihmesthales@hotmail.com>
2022-10-26 21:54:33 -0300

    renamed cambio service

--- ms-cambio/src/main/java/com/br/MsCambioApplication.java
+++ ms-cambio/src/main/java/com/br/MsCambioApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CambioServiceApplication {
+public class MsCambioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CambioServiceApplication.class, args);
+		SpringApplication.run(MsCambioApplication.class, args);
 	}
 
 }
